4ec212ada127a18a05c86915b929011740d55f53
Adam MacKinnon <adamcmackdev@gmail.com>
2022-08-21 12:04:58 -0400

    tested with sample sheet, verified working with DB configs

--- src/main/java/com/javatechie/spring/batch/controller/JobController.java
+++ src/main/java/com/javatechie/spring/batch/controller/JobController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/jobs")
-public class JobController {
+public class  JobController {
 
     @Autowired
     private JobLauncher jobLauncher;
